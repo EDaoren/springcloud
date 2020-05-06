a1f1c46d9df4ef1a2c20dbb5a44f71563222ad13
EDaoren <550790375@qq.com>
2020-05-07 07:50:03 +0800

    Spring-cloud 配置服务

---
+++ config-server-git/src/main/java/com/edaoren/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.edaoren.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * SpringCLoud 配置中心启动服务
+ * @author EDaoren
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
